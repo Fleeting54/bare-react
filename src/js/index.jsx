ee08a6803db431f8e16f0a61e608e6e7981de981
takhi <mhuynh09@gmail.com>
2018-01-28 13:20:31 +1100

    Initial bare react

---
+++ src/js/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App greeting='Hi World!'/>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.jsx',
+  output: {
+    path: path.resolve('public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            include: [path.join(__dirname, '/src/js')],
+            loader: ['babel-loader']
+        }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './src/index.html',
+        inject: 'body'
+    })
+  ]
+}
